0c6952fac02c012adaa0d0ef224d80b46a44141f
Lucas <lucasfelippemello@hotmail.com>
2022-05-31 11:41:55 -0300

    polymorphism

---
+++ Conta/Conta.js
@@ -0,0 +1,59 @@
+export class Conta{
+    constructor(saldoInicial, cliente, agencia){
+        if(this.constructor == Conta) {
+            throw new Error("Não instancie um objeto do tipo Conta diretamente!");
+        }
+        
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+    
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+
+    sacar(valor ){
+        throw new Error("O método Sacar da Conta é abstrato.")
+    }
+
+    _sacar(){
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+
+        return 0;
+    }
+
+    depositar(valor) {
+        if(valor <= 0){
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        conta.cidadee = "Curitiba";
+        valor = 20;
+    }
+
+    teste(){
+        console.log("teste")
+    }
+}
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./ContaCorrente.js";
-import {ContaPoupanca} from "./ContaPoupanca.js";
-import { Conta } from "./Conta.js";
+import { Diretor } from "./Funcionário/Diretor.js";
+import { Gerente } from "./Funcionário/Gerente.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Lucas", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Mantorras", 5000, 12378945601);
+gerente.cadastrarSenha("123");
 
-const contaCorrenteLucas = new ContaCorrente(cliente1, 1001);
-contaCorrenteLucas.depositar(500);
+const cliente = new Cliente("Jano", 789045612300, 789);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "789")
 
-const contaPoupanca = new ContaPoupanca (50, cliente1, 1001);
+console.log(gerenteEstaLogado, diretorEstaLogado);
+console.log(clienteEstaLogado);
 
-
-console.log(contaPoupanca);
 
